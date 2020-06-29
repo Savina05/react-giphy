447ddd4bcaccb9eddced65170a67c793a819c9ea
Savina Aliaj savina.aliaj@gmail.com <savina.aliaj@gmail.com>
2020-06-29 21:55:53 +0200

    refactor

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      keyword: ""
+    };
+  }
+
   handleUpdate = (event) => {
+    this.setState({
+      keyword: event.target.value
+    });
     this.props.searchFunction(event.target.value);
   }
 
   render() {
     return (
       <input
+        value={this.state.keyword}
         type="text"
         className="form-control form-search"
         onChange={this.handleUpdate}
